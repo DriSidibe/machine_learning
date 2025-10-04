7c696be97c3d52d3b6005afb7d9cd59a95d53e8a
DriSidibe <dsidibe653@gmail.com>
2025-10-04 17:52:04 +0000

    add chien chat dataset

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/drissa/Documents/deep_learning/trainning/env
+set -gx VIRTUAL_ENV /home/drissa/Documents/machine_learning/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
